62737bba3c815035b71992318439cf3daee101a8
790013438 <790013438@qq.com>
2018-09-05 14:50:38 +0800

    添加js添加数组

--- vue-shopping-list/script.js
+++ vue-shopping-list/script.js
@@ -2,10 +2,25 @@ var data = {
   items: [{ text: 'Banans', checked: true },
     { text: 'Apples', checked: false }],
   title: 'My Shopping List',
+
   newItem: ''
 };
 
 new Vue({
   el: '#app',
-  data: data
+  data: data,
+  methods: {
+    addItem: function () {
+      var text;
+
+      text = this.newItem.trim();
+      if (text) {
+        this.items.push({
+          text: text,
+          checked: false
+        });
+        this.newItem = '';
+      }
+    }
+  }
 });
