52b0d9898f9bf4a98fe3aec5b238ca71a3ed2b07
790013438 <790013438@qq.com>
2019-02-14 14:36:51 +0800

    经过计算后，再展示，computed属性

--- Pomodoro/script.js
+++ Pomodoro/script.js
@@ -18,17 +18,22 @@ new Vue({
   el: '#app',
   data: data,
   computed: {
+    // 提示状态是工作还是休息
     title: function () {
       return this.pomodoroState === POMODORO_STATES.WORK ? 'Work!' : 'Rest!'
     },
+    // 通过计算后展示分钟
     min: function () {
       if (this.minute < 10) {
+        return '0' + this.minute;
       }
 
       return this.minute;
     },
     sec: function () {
       if (this.second < 10) {
+        // 如果小于10，则在前补零
+        return '0' + this.second;
       }
 
       return this.second;
@@ -36,6 +41,7 @@ new Vue({
   },
   methods: {
     start: function () {
+      // setInterval刚开始会等待一秒后执行，一次为周期
       this._tick();
       this.interval = setInterval(this._tick, 1000);
     },
@@ -47,8 +53,9 @@ new Vue({
       } else {
         // else if second is 0 and minute is not 0, decrement minute and set second to 59
         if (this.minute !== 0) {
+          // 如果不为零则倒数
           this.minute--;
-          // 分钟减一后，秒钟重新计数
+          // 分钟减一后，秒钟重新从59开始
           this.second = 59;
           return;
         } else {
