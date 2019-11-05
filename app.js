13017528d803e258ce0c232b4775bd97acb39645
790013438 <790013438@qq.com>
2019-11-05 17:07:07 +0800

    遍历对象的key 和 value， map的key 和 value

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ new Vue({
   el: '#app',
   data: {
     show: true,
-    ingredients: ['meat', 'fruit', 'cookies']
+    ingredients: ['meat', 'fruit', 'cookies'],
+    persons: [
+      {name: 'Max', age: 27, color: 'red'},
+      {name: 'Anna', age: 'unknown', color: 'blue'}
+    ]
   }
 });
