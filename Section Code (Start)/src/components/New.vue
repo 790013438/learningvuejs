3ec7a09ed842bdd569b8fa1e691f4b99f0deea25
790013438 <790013438@qq.com>
2019-11-12 15:45:20 +0800

    动态组件，切换到其他的组件，没有选择的组件会被销毁，再次选择组件会重新创建

--- Section Code (Start)/src/components/New.vue
+++ Section Code (Start)/src/components/New.vue
@@ -1,3 +1,21 @@
 <template>
-  <p>New</p>
+  <div>
+    <p>New</p>
+    <h2>The New</h2>
+    <button @click="count++">Increase</button>
+    <p>{{count}}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      count: 0
+    };
+  },
+  destroyed: function() {
+    console.log('destroyed!');
+  }
+}
+</script>
