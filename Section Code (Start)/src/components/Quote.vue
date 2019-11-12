c5cb86fd4301f8968762ba1bcb3d97bd66743a54
790013438 <790013438@qq.com>
2019-11-12 11:13:29 +0800

    调整样式

---
+++ Section Code (Start)/src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>A wonder full Quto</p>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+div {
+  border: 1px solid black;
+  margin-top: 30px;
+  padding: 24px 12px;
+}
+p {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
