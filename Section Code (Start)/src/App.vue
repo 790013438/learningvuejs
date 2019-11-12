b6fe5e4e7ae4a7b9b7b9e3b3b5a59dfd83e76459
790013438 <790013438@qq.com>
2019-11-12 15:29:24 +0800

    动态选择模板

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -2,15 +2,13 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <app-quote>
-          <template v-slot:title>
-            <h1>{{quoteTitle}}</h1>
-          </template>
-          <template v-slot:subtitle>a subtitle</template>
-          <template>
-            <p>A wonderful Quote</p>
-          </template>
-        </app-quote>
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <p>{{selectedComponent}}</p>
+        <component :is="selectedComponent">
+          <p>default content</p>
+        </component>
       </div>
     </div>
   </div>
@@ -18,15 +16,20 @@
 
 <script>
 import Quote from './components/Quote.vue'
+import Author from './components/Author.vue'
+import New from './components/New.vue'
 
 export default {
   data: function() {
     return {
-      quoteTitle: 'The Quote Title'
+      quoteTitle: 'The Quote Title',
+      selectedComponent: 'appQuote'
     }
   },
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor: Author,
+    appNew: New
   }
 }
 </script>
