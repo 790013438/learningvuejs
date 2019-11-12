e172d2e20c473a74ddb92d11bf329f05444aac96
790013438 <790013438@qq.com>
2019-11-12 11:44:14 +0800

    不使用绑定数据，子组件从父组件那里获取属性

--- Section Code (Start)/src/components/Quote.vue
+++ Section Code (Start)/src/components/Quote.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
-    <p>A wonder full Quto</p>
+    <p>{{quote}}</p>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ['quote']
+}
 </script>
 
 <style scoped>
 div {
   border: 1px solid black;
+  box-shadow: 1px 1px 2px black;
   margin-top: 30px;
   padding: 24px 12px;
+  text-align: center;
 }
 p {
   display: flex;
