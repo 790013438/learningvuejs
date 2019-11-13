fdd7f59aadccec7efd2973780658e1f295fd8f26
790013438 <790013438@qq.com>
2019-11-13 12:35:51 +0800

    修改没有内容不添加

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -2,7 +2,7 @@
   <div class="row">
     <form>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
-        <textarea class="form-control" v-model="quote" rows="3"></textarea>
+        <textarea id="quote" class="form-control" v-model="quote" rows="3"></textarea>
       </div>
       <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3 form-group">
         <button class="btn btn-primary" @click.prevent="addNewQuote">Add New Quote</button>
@@ -20,8 +20,11 @@ export default {
   },
   methods: {
     addNewQuote: function() {
-      this.$emit('addNewQuote', this.quote);
-      this.quote = '';
+      if (this.quote) {
+        this.$emit('addNewQuote', this.quote);
+        this.quote = '';
+        document.getElementById('quote').focus();
+      }
     }
   }
 }
