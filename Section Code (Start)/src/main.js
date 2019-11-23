2d54c9325306a003b1dcb81ba90ead4a63716d56
790013438 <790013438@qq.com>
2019-11-23 17:48:19 +0800

    设置后缀修饰

--- Section Code (Start)/src/main.js
+++ Section Code (Start)/src/main.js
@@ -3,13 +3,19 @@ import App from './App.vue'
 
 Vue.directive('highlight', {
   bind(el, binding, vnode) {
-//    el.style.backgroundColor = 'lightgreen'
-//    el.style.backgroundColor = binding.value;
-    if (binding.arg === 'backgroundColor') {
-      el.style.backgroundColor = binding.value;
-    } else {
-      el.style.color = binding.value;
+    //    el.style.backgroundColor = 'lightgreen'
+    //    el.style.backgroundColor = binding.value;
+    let delay = 0;
+    if (binding.modifiers['delay']) {
+      delay = 3000;
     }
+    setTimeout(()=>{
+      if (binding.arg === 'backgroundColor') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
   }
 });
 new Vue({
