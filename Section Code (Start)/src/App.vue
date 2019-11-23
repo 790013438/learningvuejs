942cd01090810f54312901299208c80da09abecf
790013438 <790013438@qq.com>
2019-11-23 18:11:24 +0800

    设置本地的指令

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -12,6 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Cunstom Directives</h1>
         <p v-highlight:backgroundColor="'lightcoral'">Color this</p>
+        <p v-local-highlight:backgroundColor.delay="'lightcoral'">Color this</p>
       </div>
     </div>
   </div>
@@ -19,6 +20,23 @@
 
 <script>
 export default {
+  directives: {
+    'local-highlight': {
+      bind: function(el, binding, vnode) {
+        let delay = 0;
+        if (binding.modifiers['delay']) {
+          delay = 3000;
+        }
+        setTimeout(()=>{
+          if (binding.arg === 'backgroundColor') {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, delay);
+      }
+    }
+  }
 }
 </script>
 
