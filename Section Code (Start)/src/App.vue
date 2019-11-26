75582bb5326248a97fac87f600fcd97e1543eae5
790013438 <790013438@qq.com>
2019-11-26 18:13:45 +0800

    用computed代替filter

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -4,6 +4,11 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <p>{{text | toUpperCase | to-lowercase}}</p>
+        <hr/>
+        <ul>
+          <input v-model="filter"/>
+          <li v-for="fruit in fruitesFiltered">{{fruit}}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -13,12 +18,22 @@
 export default {
   data: function() {
     return {
-      text: 'Hello there!'
+      text: 'Hello there!',
+      filter: '',
+      fruits: ['Apple', 'Blanana', 'Orange', 'Pineappble']
     };
   },
   filters: {
     toUpperCase: function(value) {
       return value.toUpperCase();
+    }
+  },
+  computed: {
+    fruitesFiltered: function() {
+      return this.fruits.filter((item) => {
+        let regExp = new RegExp(this.filter, 'gi');
+        return regExp.test(item);
+      });
     }
   }
 }
