bf403f745b14696077e872003aeca490bf4175d7
790013438 <790013438@qq.com>
2019-11-28 18:01:15 +0800

    使用animate.css 可以在enter-active-class 里面直接设置加载时候的动画
    在leave-active-class 里面设置去除页面时候的动画
    
    直接设置动画是用 name 和 css层叠样式表 name-leave-active,设置动画过渡
    
    可以用vue属性绑定设置动画的名字

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -3,20 +3,33 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
+        <hr/>
+        <select class="form-control"
+          v-model="alertAnimation">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br/></br/>
         <button class="btn btn-primary"
           @click="show = !show">Show Alert</button>
         <br/><br/>
-        <transition name="fade">
+        <transition :name="alertAnimation">
         <div class="alert alert-info"
              v-if="show">This is some Info</div>
         </transition>
-        <transition name="slide" type="animation">
+        <transition :name="alertAnimation" type="animation">
         <div class="alert alert-info"
              v-if="show">This is some Info</div>
         </transition>
-        <transition name="fade" appear>
+        <transition :name="alertAnimation" appear>
         <div class="alert alert-info"
              v-if="show">This is some Info</div>
+        </transition>
+        <transition
+          enter-active-class="animated bounce"
+          leave-active-class="animated shake">
+        <div class="alert alert-info"
+             v-show="show">This is some Info</div>
         </transition>
       </div>
     </div>
@@ -27,7 +40,8 @@
 export default {
   data() {
     return {
-      show: true
+      show: true,
+      alertAnimation: 'fade'
     };
   }
 }
