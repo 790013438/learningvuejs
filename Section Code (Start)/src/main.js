eb2e2f7bee5c15133a84a9685fef27ce9a2df466
790013438 <790013438@qq.com>
2019-12-05 13:02:10 +0800

    添加监听

--- Section Code (Start)/src/main.js
+++ Section Code (Start)/src/main.js
@@ -4,6 +4,13 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource)
 Vue.http.options.root = 'https://vuejs-http-61e6e.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next)=>{
+  console.log(request)
+  if (request.method === 'POST') {
+    request.method = 'PUT'
+  }
+  next();
+})
 
 new Vue({
   el: '#app',
