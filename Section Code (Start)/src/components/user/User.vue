132b5ac1cd8b159e0c5bf6656546ae4c0131c651
790013438 <790013438@qq.com>
2019-12-09 18:50:08 +0800

    设置id绑定route属性

--- Section Code (Start)/src/components/user/User.vue
+++ Section Code (Start)/src/components/user/User.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <h1>The User Page</h1>
+    <hr/>
+    <p>Loaded ID: {{id}}</p>
     <button @click="navHome" class="btn btn-primary">Go Home</button>
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      id: this.$route.params.id
+    };
+  },
   methods: {
     navHome: function() {
       this.$router.push('/');
