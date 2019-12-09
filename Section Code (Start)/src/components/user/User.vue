f12469496fd54cbfaff2100cd30d702d33fb90dc
790013438 <790013438@qq.com>
2019-12-09 13:52:06 +0800

    在esscript里面设置导航

--- Section Code (Start)/src/components/user/User.vue
+++ Section Code (Start)/src/components/user/User.vue
@@ -1,4 +1,16 @@
 <template>
+  <div>
     <h1>The User Page</h1>
-
+    <button @click="navHome" class="btn btn-primary">Go Home</button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    navHome: function() {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
