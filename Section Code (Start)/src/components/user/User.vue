294c01e6f4335d01fa3d8a8dd7fa49c10ca2eaee
790013438 <790013438@qq.com>
2019-12-10 13:52:01 +0800

    设置子router

--- Section Code (Start)/src/components/user/User.vue
+++ Section Code (Start)/src/components/user/User.vue
@@ -2,8 +2,9 @@
   <div>
     <h1>The User Page</h1>
     <hr/>
-    <p>Loaded ID: {{id}}</p>
     <button @click="navHome" class="btn btn-primary">Go Home</button>
+    <hr/>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -13,11 +14,6 @@ export default {
     return {
       id: this.$route.params.id
     };
-  },
-  watch: {
-    '$route'(to, from) {
-      this.id = to.params.id;
-    }
   },
   methods: {
     navHome: function() {
