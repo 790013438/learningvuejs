66e3f5f1bc36ed870cacad48cc2966f4032385af
790013438 <790013438@qq.com>
2019-12-10 17:55:30 +0800

    用冒号绑定属性，动态组装router-link的to属性

--- Section Code (Start)/src/components/user/UserDetail.vue
+++ Section Code (Start)/src/components/user/UserDetail.vue
@@ -2,5 +2,6 @@
   <div>
     <h3>Some User Details</h3>
     <p>User Loaded has id : {{$route.params.id}}</p>
+    <router-link tag="button" :to="'/user/' + $route.params.id + '/edit'" class="btn btn-info">Edit User</router-link>
   </div>
 </template>
