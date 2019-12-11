c2e1706ebf96bdf4d0301651adb840d71fa28bc2
790013438 <790013438@qq.com>
2019-12-11 13:37:34 +0800

    设置多个router-view并添加名字

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -4,8 +4,9 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Routing</h1>
         <hr/>
-        <appHeader></appHeader>
+        <router-view name="app-header"></router-view>
         <router-view></router-view>
+        <router-view name="app-bottom"></router-view>
       </div>
     </div>
   </div>
--- Section Code (Start)/src/routes.js
+++ Section Code (Start)/src/routes.js
@@ -3,12 +3,20 @@ import UserStart from './components/user/UserStart.vue'
 import UserDetail from './components/user/UserDetail.vue'
 import UserEdit from './components/user/UserEdit.vue'
 import Home from './components/Home.vue'
+import Header from './components/Header.vue'
 
 export {routes}
 
 const routes = [
-  {path: '', component: Home},
-  {path: '/user', component: User, children: [
+  {path: '', components: {
+    default: Home,
+    'app-header': Header
+  }},
+  {path: '/user', components: {
+    default: User,
+    'app-bottom': Header
+  },
+    children: [
     {path: '', component:UserStart},
     {path: ':id', component:UserDetail},
     {path: ':id/edit', component: UserEdit, name: 'userEdit'}
