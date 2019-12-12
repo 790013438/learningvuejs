6500a2e5def6e3b94b8e46f764e4722df8380786
790013438 <790013438@qq.com>
2019-12-12 16:57:08 +0800

    hash-fragment锚点

--- Section Code (Start)/src/components/user/UserDetail.vue
+++ Section Code (Start)/src/components/user/UserDetail.vue
@@ -3,9 +3,24 @@
     <h3>Some User Details</h3>
     <p>User Loaded has id : {{$route.params.id}}</p>
     <router-link tag="button"
-                 :to="{name: 'userEdit', params: {id: $route.params.id}, query: {locale: 'en', q: 100}}"
+                 :to="link"
       class="btn btn-info">Edit User</router-link>
     <br/>
     <br/>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      link: {
+        name: 'userEdit',
+        params: {id: this.$route.params.id},
+        query: {locale: 'en', q: 100},
+        hash: '#data'
+      }
+    };
+  }
+}
+</script>
--- Section Code (Start)/src/components/user/UserEdit.vue
+++ Section Code (Start)/src/components/user/UserEdit.vue
@@ -3,5 +3,7 @@
     <h3>Edit the User</h3>
     <p>Locale: {{$route.query.locale}}</p>
     <p>analasis: {{$route.query.q}}</p>
+    <div style="height: 700px"></div>
+    <p id="data">Some data Text</p>
   </div>
 </template>
