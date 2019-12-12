76cf5b53934ca9fc452232721f9b9e5d272f2ff0
790013438 <790013438@qq.com>
2019-12-12 15:39:37 +0800

    router-view动画

--- Section Code (Start)/src/App.vue
+++ Section Code (Start)/src/App.vue
@@ -5,7 +5,9 @@
         <h1>Routing</h1>
         <hr/>
         <router-view name="app-header"></router-view>
+        <transition name="slide" mode="out-in" appear type="animation">
         <router-view></router-view>
+        </transition>
         <router-view name="app-bottom"></router-view>
       </div>
     </div>
@@ -23,4 +25,35 @@ export default {
 </script>
 
 <style>
+.slide-enter {
+  opacity: 0;
+}
+.slide-enter-active {
+  transition: opacity .71s ease-in;
+  animation: slide-in .70s ease-in;
+}
+.slide-leave {
+  opacity: 1;
+  transform: translateY(0);
+}
+.slide-leave-active {
+  animation: slide-out .75s ease-out;
+}
+@keyframes slide-in {
+  from {
+    transform:translateY(-30px);
+  }
+  to {
+    transform:translateY(0);
+  }
+}
+@keyframes slide-out {
+  from {
+    transform:translateY(0);
+  }
+  to {
+    transform:translateY(-30px);
+    opacity: 0;
+  }
+}
 </style>
