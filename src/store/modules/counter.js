5f1fe1df14be543a963c4ba7a06c6e9ef1833155
790013438 <790013438@qq.com>
2020-01-15 14:01:05 +0800

    使用modules

---
+++ src/store/modules/counter.js
@@ -0,0 +1,49 @@
+const state = {
+  counter: 0
+}
+const getters = {
+  doubleCounter: state => (state.counter * 2),
+  clickString: state => `${state.counter} Clicks`,
+}
+const mutations = {
+  increase: (state, payload) => {
+    if(payload) {
+      state.counter += payload;
+    } else {
+      state.counter++;
+    }
+  },
+  decrease: (state, payload) => {
+    if (payload) {
+      state.counter -= Number(payload);
+    } else {
+      state.counter--;
+    }
+  }
+}
+
+const actions = {
+  increase: (context, payload) => {
+    context.commit('increase', payload);
+  },
+  decrease: ({commit}) => {
+    commit('decrease');
+  },
+  asycnIncrease: ({commit}, payload) => {
+    setTimeout(function() {
+      commit('increase', payload.by)
+    }, payload.timeout);
+  },
+  asycnDecrease: ({commit}, payload) => {
+    setTimeout(()=> {
+      commit('decrease', payload.by);
+    }, payload.timeout);
+  }
+}
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,58 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import counter from './modules/counter.js'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
-    counter: 0,
     value: 0
   },
   getters: {
-    doubleCounter: state => (state.counter * 2),
-    clickString: state => `${state.counter} Clicks`,
     value: function(state) {
       return state.value;
     }
   },
   mutations: {
-    increase: (state, payload) => {
-      if(payload) {
-        state.counter += payload;
-      } else {
-        state.counter++;
-      }
-    },
-    decrease: (state, payload) => {
-      if (payload) {
-        state.counter -= Number(payload);
-      } else {
-        state.counter--;
-      }
-    },
     updateValue: (state, payload) => {
       state.value = payload;
     }
   },
   actions: {
-    increase: (context, payload) => {
-      context.commit('increase', payload);
-    },
-    decrease: ({commit}) => {
-      commit('decrease');
-    },
-    asycnIncrease: ({commit}, payload) => {
-      setTimeout(function() {
-        commit('increase', payload.by)
-      }, payload.timeout);
-    },
-    asycnDecrease: ({commit}, payload) => {
-      setTimeout(()=> {
-        commit('decrease', payload.by);
-      }, payload.timeout);
-    },
     updateValue: (context, payload) => {
       context.commit('updateValue', payload);
     }
+  },
+  modules: {
+    counter
   }
 })
