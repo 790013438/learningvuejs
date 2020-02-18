93a564de11e4d358c70f49b5b4b6d512ecba0bfb
790013438 <790013438@qq.com>
2020-02-19 00:47:13 +0800

    定义buy操作

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="col-sm-6 col-md-4">
     <div class="card mb-3">
-      <div class="card-header bg-success" style="color:#fff">NAME
-        <small>(Price: PRICE)</small></div>
+      <div class="card-header bg-success" style="color:#fff">{{stock.name}}
+        <small>(Price: {{stock.price}})</small></div>
       <div class="card-body">
         <div class="row">
           <div style="margin-left:auto">
-            <input type="number" class="form-control" placeholder="Quantity">
+            <input type="number" class="form-control" placeholder="Quantity"
+            v-model="quantity">
           </div>
-          <button class="btn btn-success" style="margin:auto">Buy</button>
+          <button class="btn btn-success" style="margin:auto"
+                                          @click="buy"
+                                          :disabled="disabled">Buy</button>
         </div>
       </div>
     </div>
@@ -17,7 +20,28 @@
 
 <script>
 export default {
-  props: ['stock']
+  props: ['stock'],
+  data() {
+    return {
+      quantity: 0
+    }
+  },
+  methods: {
+    buy() {
+      const order = {
+        id: this.stock.id,
+        name: this.stock.name,
+        price: this.stock.price,
+        quantity: this.quantity
+      }
+      console.log(order);
+    }
+  },
+  computed: {
+    disabled() {
+      return this.quantity <= 0 || Math.floor(this.quantity) != this.quantity;
+    }
+  }
 }
 </script>
 
